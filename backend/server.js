443e37c54d3d14dcb3ee74b9479d2752eac77054
Caio <caiosilvage05@gmail.com>
2026-09-14 21:46:46 -0300

    feat: adicionando arquivs

--- backend/server.js
+++ backend/server.js
@@ -1138,4 +1138,4 @@ app.get('/busca', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`✅ Servidor rodando na porta ${PORT}`));
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
